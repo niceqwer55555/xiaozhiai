042db0e501e10f5d07f9bb80455c37e0eb4bb8ba
niceqwer55555 <347921622@qq.com>
2025-02-20 15:03:13 +0800

    1

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-from kivymd.app import MDApp
-from kivy.lang import Builder
+import os
+import sys
 
-class SampleApp(MDApp):
-    
-    def build(self):
-        self.appKv='''
-MDScreen:
-    MDLabel:
-        text:'Hello,World.'
-        multiline:True
-        color:"blue"
-        halign:'center'         
-'''
-        AppScreen=Builder.load_string(self.appKv)
-        return AppScreen
+# 确保 src/ 目录可以被 Python 识别
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
-SampleApp().run()
-    
+from src.ota import get_ota_version
+from src.mqtt_client import MQTTClient
+from src.gui import GUI
+import logging
+# ✅ 配置全局 logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),  # 输出到控制台
+        logging.FileHandler("app.log", encoding="utf-8")  # 记录到日志文件
+    ]
+)
+
+def main():
+    # 测试日志
+    logging.info("✅ 日志系统已初始化")
+    """程序入口"""
+    # 获取 OTA 版本 & MQTT 服务器信息
+    get_ota_version()
+
+    # 启动 MQTT
+    mqtt_client = MQTTClient()
+
+    # # 启动 GUI
+    gui = GUI(mqtt_client=mqtt_client)
+    mqtt_client.gui = gui
+
+
+if __name__ == "__main__":
+    main()
